e0cc391e476d3777c187f5dc8bc583c9f52cb61a
Ninka23 <ninobenidze23@gmail.com>
2025-03-28 18:08:03 +0400

    create new branch

---
+++ Task.js
@@ -0,0 +1,52 @@
+/*let a=87;
+let b=65;
+console.log(a+b)*/
+
+
+
+/*let student={
+    firstname: "Nino",
+    lastname: "Benidze",
+    Age:27,
+    email: "ninobenidze23@gmail.com",
+    marks: [100, 91, 90]
+
+};
+
+console.log(student.firstname)*/
+
+
+/*let m=45;
+let n=23;
+
+if(m>n)
+    {console.log(m+n);}
+else {console.log(n);}*/
+
+
+/*let studentname = "Nino";
+console.log("Rise and shine," + studentname +"!");*/
+
+
+/*let salary= 1800;
+let bonus;
+
+if (salary>=2000){bonus=0;}
+else {bonus=salary*0.10;}
+console.log("bonus", bonus);*/
+
+
+/*let a=23;
+let b=a/2;
+let c=a+b;
+
+console.log(c); */
+
+
+let phones = ["iPhone", "samsung", "pixel", "huawei","xiaomi"];
+console.log(phones);
+console.log(phones[0], phones[phones.length-1]);
+
+
+
+
